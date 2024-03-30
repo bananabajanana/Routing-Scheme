d313476c4f8e40540af2644930a26a87924ea0c7
bananabajanana <ohad.heines@gmail.com>
2024-03-30 20:43:28 +0300

    Added 'Top' Classes

---
+++ Routing-Scheme-Simulation/src/simulator/RoutingGraphBuilder.java
@@ -0,0 +1,13 @@
+package simulator;
+
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultEdge;
+
+/**
+ * Builder class for setting up the simulator graphs.
+ */
+public class RoutingGraphBuilder {
+  public Graph<ComputerNode, DefaultEdge> newGraph() {
+    throw new RuntimeException("Not Implemented");
+  }
+}
